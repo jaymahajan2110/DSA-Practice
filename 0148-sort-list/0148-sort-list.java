b6a6d31d0776fa0de70d54c6a7a8742c3d34438e
jaymahajan2110 <jaymahajan2112@gmail.com>
2026-08-04 20:06:38 +0530

    Time: 10 ms (52.11%), Space: 59.6 MB (37.96%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    public ListNode sortList(ListNode head) {
+        // Base Case
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // Split the list into two halves
+        ListNode mid = getMid(head);
+        ListNode rightHead = mid.next;
+        mid.next = null;
+
+        // Recursively sort both halves
+        ListNode left = sortList(head);
+        ListNode right = sortList(rightHead);
+
+        // Merge the sorted halves
+        return merge(left, right);
+    }
+
+    // Finds the middle node (left middle for even length)
+    private ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    // Merge two sorted linked lists
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (l1 != null) {
+            tail.next = l1;
+        } else {
+            tail.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
