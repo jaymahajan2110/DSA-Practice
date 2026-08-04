812ea7efaaacf2c1fa30ca712e1bfb1f2a01123e
jaymahajan2110 <jaymahajan2112@gmail.com>
2026-08-04 13:29:35 +0530

    Time: 1 ms (76.63%), Space: 42.2 MB (89.78%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,19 +1,24 @@
 class Solution {
-    private int square(int number){
+
+    private int square(int number) {
         int sum = 0;
-            while (number>0){
-            int rem = number%10;
-            sum += rem*rem;
-            number = number/10;}
-            return sum;
+        while (number > 0) {
+            int rem = number % 10;
+            sum += rem * rem;
+            number /= 10;
         }
+        return sum;
+    }
+
     public boolean isHappy(int n) {
-        int slow = square(n);
-        int fast = square(square(n));
+        int slow = n;
+        int fast = n;
+
         do {
-    slow = square(slow);
-    fast = square(square(fast));
-} while (slow != fast);
+            slow = square(slow);
+            fast = square(square(fast));
+        } while (slow != fast);
+
         return slow == 1;
     }
 }
