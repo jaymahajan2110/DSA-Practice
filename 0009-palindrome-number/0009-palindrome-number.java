ab330c22d948d85a55f9e5f397d285e347d4469b
jaymahajan2110 <jaymahajan2112@gmail.com>
2026-08-11 00:35:11 +0530

    Time: 6 ms (21.51%), Space: 46.3 MB (6.06%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,17 +1,23 @@
 class Solution {
     public boolean isPalindrome(int x) {
         String str1 = String.valueOf(x);
+
+        if (str1.charAt(0) == '-') {
+            return false;
+        }
+
         int start = 0;
-        int end = str1.length()-1;
-        for (int i =0;i<str1.length();i++){
-            if (str1.charAt(0)=='-'){
-                return false;
-            }else if (str1.charAt(start)!=str1.charAt(end)){
+        int end = str1.length() - 1;
+
+        while (start < end) {
+            if (str1.charAt(start) != str1.charAt(end)) {
                 return false;
             }
+
             start++;
             end--;
         }
+
         return true;
     }
 }
