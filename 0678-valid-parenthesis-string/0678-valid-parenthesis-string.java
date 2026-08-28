db183ae5c0c50e000c62211fb40589453eb0c341
jaymahajan2110 <jaymahajan2112@gmail.com>
2026-08-29 02:38:07 +0530

    Time: 0 ms (100%), Space: 42.9 MB (34%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public boolean checkValidString(String s) {
+
+        int min = 0;
+        int max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                min++;
+                max++;
+            }
+
+            else if (ch == ')') {
+                min--;
+                max--;
+            }
+
+            else { // '*'
+                min--;
+                max++;
+            }
+
+            if (max < 0) {
+                return false;
+            }
+
+            min = Math.max(0, min);
+        }
+
+        return min == 0;
+    }
+}
